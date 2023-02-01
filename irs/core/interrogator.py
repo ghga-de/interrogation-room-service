f34a5e7772fee2961c279a7b69cc721fadba0582
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-02-01 11:50:21 +0000

    Finished refactoring

--- irs/core/interrogator.py
+++ irs/core/interrogator.py
@@ -41,8 +41,8 @@ from irs.ports.inbound.interrogator import InterrogatorPort
 from irs.ports.outbound.event_pub import EventPublisherPort
 
 
-class CipherSegmentProcessor:
-    """TODO"""
+class CipherSegmentProcessor:  # pylint: disable=too-many-instance-attributes
+    """Process inbox file for checksum generation and reencryption"""
 
     def __init__(
         self,
@@ -68,57 +68,18 @@ class CipherSegmentProcessor:
 
         # hashsum buffers
         self.total_sha256_checksum = hashlib.sha256()
-        self.encrypted_md5_part_checksums = list[str]
-        self.encrypted_sha256_part_checksums = list[str]
+        self.encrypted_md5_part_checksums: list[str] = []
+        self.encrypted_sha256_part_checksums: list[str] = []
 
         # counter for uploading reencrypted file to staging area
         self.reencrypted_part_number = 0
-        self.part_number = 0
 
         # buffers to accumulate ciphersegments up to part size
         self.partial_ciphersegment = b""
         self.reencryption_buffer = b""
 
-    def _get_segments(self, *, file_part: bytes) -> Tuple[List[bytes], bytes]:
-        """Chunk file part into decryptable segments"""
-
-        num_segments = len(file_part) / CIPHER_SEGMENT_SIZE
-        full_segments = int(num_segments)
-        segments = [
-            file_part[i * CIPHER_SEGMENT_SIZE : (i + 1) * CIPHER_SEGMENT_SIZE]
-            for i in range(full_segments)
-        ]
-
-        # check if we have a remainder of bytes that we need to handle,
-        # i.e. non-matching boundaries between part and cipher segment size
-        incomplete_segment = b""
-        partial_segment_idx = math.ceil(num_segments)
-        if partial_segment_idx != full_segments:
-            incomplete_segment = file_part[full_segments * CIPHER_SEGMENT_SIZE :]
-        return segments, incomplete_segment
-
-    def _get_part_checksums(self, *, file_part: bytes):
-        """Compute md5 and sha256 for encrypted part"""
-        return (
-            hashlib.md5(file_part, usedforsecurity=False).hexdigest(),
-            hashlib.sha256(file_part).hexdigest(),
-        )
-
-    def _encrypt_segment(self, *, data: bytes, key: bytes) -> bytes:
-        """
-        Utility function to generate a nonce, encrypt data with Chacha20,
-        and authenticate it with Poly1305.
-        """
-
-        nonce = os.urandom(12)
-        encrypted_data = crypto_aead_chacha20poly1305_ietf_encrypt(
-            data, None, nonce, key
-        )  # no aad
-
-        return nonce + encrypted_data
-
     async def process(self):
-        """TODO"""
+        """Delegate processing and return checksums"""
         # start multipart upload for staging multipart upload
         self.upload_id = await init_staging(object_id=self.object_id)
 
@@ -138,8 +99,46 @@ class CipherSegmentProcessor:
             self.total_sha256_checksum.hexdigest(),
         )
 
+    def _encrypt_segment(self, *, data: bytes, key: bytes) -> bytes:
+        """
+        Utility function to generate a nonce, encrypt data with Chacha20,
+        and authenticate it with Poly1305.
+        """
+
+        nonce = os.urandom(12)
+        encrypted_data = crypto_aead_chacha20poly1305_ietf_encrypt(
+            data, None, nonce, key
+        )  # no aad
+
+        return nonce + encrypted_data
+
+    def _get_part_checksums(self, *, file_part: bytes):
+        """Compute md5 and sha256 for encrypted part"""
+        return (
+            hashlib.md5(file_part, usedforsecurity=False).hexdigest(),
+            hashlib.sha256(file_part).hexdigest(),
+        )
+
+    def _get_segments(self, *, file_part: bytes) -> Tuple[List[bytes], bytes]:
+        """Chunk file part into decryptable segments"""
+
+        num_segments = len(file_part) / CIPHER_SEGMENT_SIZE
+        full_segments = int(num_segments)
+        segments = [
+            file_part[i * CIPHER_SEGMENT_SIZE : (i + 1) * CIPHER_SEGMENT_SIZE]
+            for i in range(full_segments)
+        ]
+
+        # check if we have a remainder of bytes that we need to handle,
+        # i.e. non-matching boundaries between part and cipher segment size
+        incomplete_segment = b""
+        partial_segment_idx = math.ceil(num_segments)
+        if partial_segment_idx != full_segments:
+            incomplete_segment = file_part[full_segments * CIPHER_SEGMENT_SIZE :]
+        return segments, incomplete_segment
+
     async def _process_parts(self):
-        """ """
+        """High-level part processing, chunking into ciphersegments"""
 
         async for part in retrieve_parts(
             url=self.download_url,
@@ -147,8 +146,6 @@ class CipherSegmentProcessor:
             part_size=self.part_size,
             offset=self.offset,
         ):
-            self.part_number += 1
-
             if self.partial_ciphersegment:
                 part = self.partial_ciphersegment + part
             ciphersegments, incomplete_ciphersegment = self._get_segments(
@@ -160,14 +157,16 @@ class CipherSegmentProcessor:
         await self._process_remaining(incomplete_ciphersegment=incomplete_ciphersegment)
 
     async def _process_segments(self, *, ciphersegments: List[bytes]):
-        """TODO"""
+        """Process complete ciphersegments"""
         for ciphersegment in ciphersegments:
             try:
                 decrypted = decrypt_block(
                     ciphersegment=ciphersegment, session_keys=[self.secret]
                 )
             except Exception as exc:
-                raise SegmentCorruptedError(part_number=self.part_number) from exc
+                raise SegmentCorruptedError(
+                    part_number=self.reencrypted_part_number
+                ) from exc
             self.total_sha256_checksum.update(decrypted)
 
             # reencrypt using the new secret
@@ -176,26 +175,10 @@ class CipherSegmentProcessor:
             )
 
         if len(self.reencryption_buffer) >= self.part_size:
-            part = self.reencryption_buffer[: self.part_size]
-
-            # calculate re-encrypted checksums
-            md5sum, sha256sum = self._get_part_checksums(file_part=part)
-            self.encrypted_md5_part_checksums.append(md5sum)
-            self.encrypted_sha256_part_checksums.append(sha256sum)
-
-            self.reencrypted_part_number += 1
-            await stage_part(
-                upload_id=self.upload_id,
-                object_id=self.object_id,
-                data=part,
-                part_number=self.reencrypted_part_number,
-            )
-
-            rest_len = len(self.reencryption_buffer) - self.part_size
-            self.reencryption_buffer = self.reencryption_buffer[-rest_len:]
+            await self._reencrypt_and_stage()
 
     async def _process_remaining(self, *, incomplete_ciphersegment: bytes):
-        """Compute total unencrypted file checksum and encrypted part checksums"""
+        """Process last, possibly incomplete ciphersegment"""
 
         if incomplete_ciphersegment:
             try:
@@ -213,26 +196,16 @@ class CipherSegmentProcessor:
             )
 
         if len(self.reencryption_buffer) > self.part_size:
+            await self._reencrypt_and_stage()
+
+        await self._reencrypt_and_stage(consume_remainder=True)
+
+    async def _reencrypt_and_stage(self, *, consume_remainder: bool = False):
+        """Reencrypt, calculate checksums and stage file part"""
+        if consume_remainder:
+            part = self.reencryption_buffer
+        else:
             part = self.reencryption_buffer[: self.part_size]
-
-            # calculate re-encrypted checksums
-            md5sum, sha256sum = self._get_part_checksums(file_part=part)
-            self.encrypted_md5_part_checksums.append(md5sum)
-            self.encrypted_sha256_part_checksums.append(sha256sum)
-
-            self.reencrypted_part_number += 1
-            await stage_part(
-                upload_id=self.upload_id,
-                object_id=self.object_id,
-                data=part,
-                part_number=self.reencrypted_part_number,
-            )
-
-            rest_len = len(self.reencryption_buffer) - self.part_size
-            self.reencryption_buffer = self.reencryption_buffer[-rest_len:]
-
-        # re-encrypt the last (incomplete) part
-        part = self.reencryption_buffer
 
         # calculate re-encrypted checksums
         md5sum, sha256sum = self._get_part_checksums(file_part=part)
@@ -246,6 +219,10 @@ class CipherSegmentProcessor:
             data=part,
             part_number=self.reencrypted_part_number,
         )
+
+        if not consume_remainder:
+            rest_len = len(self.reencryption_buffer) - self.part_size
+            self.reencryption_buffer = self.reencryption_buffer[-rest_len:]
 
 
 class Interrogator(InterrogatorPort):
